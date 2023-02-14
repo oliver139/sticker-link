9da7a08c718d0736b77fd55fe093163c7ac42f94
Oliver Mak <oliver139.working@gmail.com>
2023-02-15 01:28:41 +0800

    i18n composition api mode

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -5,6 +5,9 @@ const locale = localStorage.site_lang || "zh-hk";
 localStorage.site_lang = locale;
 
 const i18n = createI18n({
+  // Composition API
+  legacy: false,
+
   // default locale
   locale: locale,
 
