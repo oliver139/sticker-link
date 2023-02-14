2c21004e7b3a08a376db51002c2a3d655b3a044a
Oliver Mak <oliver139.working@gmail.com>
2023-02-14 14:00:53 +0800

    Icon library

---
+++ src/utils/iconImport.js
@@ -0,0 +1,23 @@
+// Oh Vue Icon https://oh-vue-icons.js.org/
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import {
+  BiCursorFill,
+  BiZoomIn,
+  FaPaw,
+  FaTwitter,
+  FaYoutube,
+  IoLanguage,
+  HiSolidCake,
+} from "oh-vue-icons/icons";
+
+addIcons(
+  BiCursorFill,
+  BiZoomIn,
+  FaPaw,
+  FaTwitter,
+  FaYoutube,
+  IoLanguage,
+  HiSolidCake,
+);
+
+export default OhVueIcon;
