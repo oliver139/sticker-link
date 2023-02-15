9e9c56e13eb00a6e88a1556f3d14807369b114e0
Oliver Mak <oliver139.working@gmail.com>
2023-02-15 13:48:56 +0800

    Auto import useful composition API keywords

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,23 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+// AutoImport Composition API methods
+import AutoImport from "unplugin-auto-import/vite";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ["vue"],
+      vueTemplate: true,
+      // eslintrc: {
+      //   enabled: true,
+      //   filepath: "./.eslintrc-auto-import.json", // Default `./.eslintrc-auto-import.json`
+      //   globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      // },
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
