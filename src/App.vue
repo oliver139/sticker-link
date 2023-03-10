0c269ffaf32d13b7c412ab3adf6a3513abadb1f8
Oliver Mak <oliver139.working@gmail.com>
2023-03-10 20:08:18 +0800

    Change data handling

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,71 @@
 <template>
-  <div>
+  <div
+    id="app-wrapper"
+    :style="appStyle"
+  >
     <Navbar />
-    <hr />
-    <router-view />
+    <header class="list-logo">
+      <img :src="currentView.logo" :alt="currentView.name">
+    </header>
+    <main class="list-content">
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import viewData from "./data/viewData";
 import Navbar from "./components/Navbar.vue";
-import { useViewStore } from "@/stores/view";
 
-const view = useViewStore();
+const route = useRoute();
+
+// Get view setting
+const currentView = computed(() => {
+  const imageUrl = new URL(`/src/assets/img/${route.name}/logo.png`, import.meta.url).pathname;
+  return {
+    name: route.name,
+    logo: imageUrl,
+    background: viewData[route.name]?.background,
+  };
+});
+// background
+const appStyle = computed(() => {
+  let styles = {};
+  console.log(currentView.value);
+  switch (currentView.value.background?.type) {
+    case "image":
+      styles = {
+        "--bg-img": `url(${new URL(`/src/assets/img/${route.name}/background.jpg`, import.meta.url).pathname})`
+      };
+      break;
+
+    case "color":
+      styles = {
+        "--bg-color": currentView.value.background?.value
+      };
+      break;
+
+    default:
+      styles = {
+        "--bg-color": "black"
+      };
+      break;
+  }
+  console.log(styles);
+  return styles;
+});
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="postcss" scoped>
 
+#app-wrapper {
+  background-color: var(--bg-color);
+  background-image: var(--bg-img);
+  background-repeat: no-repeat;
+  background-position: right bottom;
+  background-size: cover;
+}
 </style>
---
+++ src/data/viewData.js
@@ -0,0 +1,14 @@
+export default {
+  cinnamoroll: {
+    "background": {
+      type: "image",
+      value: "",
+    }
+  },
+  // capoo: {
+  //   "background": {
+  //     type: "color",
+  //     value: "black",
+  //   }
+  // }
+};
