9856d193e51e522df282e2b8baf1353a96c05484
Oliver Mak <oliver139.working@gmail.com>
2023-07-08 18:46:05 +0800

    Pooh data missing

--- src/data/Pooh.js
+++ src/data/Pooh.js
@@ -1,1 +1,14 @@
-export default new Set([]);
+export default new Set([
+  {
+    text: "Pooh 1",
+    href: "https://whatsticker.online/p/5920938edu8qX/HK/zh"
+  },
+  {
+    text: "Pooh 2",
+    href: "https://whatsticker.online/p/592094oevtJAQ/HK/zh"
+  },
+  {
+    text: "Pooh 3",
+    href: "https://whatsticker.online/p/592095lh2dPru/HK/zh"
+  },
+]);
