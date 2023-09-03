dff9d10a3175e19e716eb6ee5b612aea011fa66c
Oliver Mak <oliver139.working@gmail.com>
2023-09-03 15:45:54 +0800

    Fix typo

--- src/data/DttoFriends.js
+++ src/data/DttoFriends.js
@@ -1,56 +1,56 @@
 export default new Set([
   {
-    id: "DittoFriends1",
+    id: "DttoFriends1",
     text: "dttofriends 爆可愛初登場！",
     href: "https://whatsticker.online/p/640486kzl33IK/HK/zh"
   },
   {
-    id: "DittoFriends2",
+    id: "DttoFriends2",
     text: "dtto friends動起來!日常用語",
     href: "https://whatsticker.online/p/640489Tkiy8En/HK/zh"
   },
   {
-    id: "DittoFriends3",
+    id: "DttoFriends3",
     text: "♡ dttofriends 3 ♡ 迷因梗圖第一彈 Part 1",
     href: "https://whatsticker.online/p/640491WteeveT/HK/zh"
   },
   {
-    id: "DittoFriends4",
+    id: "DttoFriends4",
     text: "♡ dttofriends 3 ♡ 迷因梗圖第一彈 Part 2",
     href: "https://whatsticker.online/p/6404926Vfeg37/HK/zh"
   },
   {
-    id: "DittoFriends5",
+    id: "DttoFriends5",
     text: "♡ dttofriends 4 ♡ 迷因梗圖第二彈 Part 1",
     href: "https://whatsticker.online/p/640494dKyslJ6/HK/zh"
   },
   {
-    id: "DittoFriends6",
+    id: "DttoFriends6",
     text: "♡ dttofriends 4 ♡ 迷因梗圖第二彈 Part 2",
     href: "https://whatsticker.online/p/640495iFJhPzk/HK/zh"
   },
   {
-    id: "DittoFriends7",
+    id: "DttoFriends7",
     text: "dtto friends 校園戀愛篇",
     href: "https://whatsticker.online/p/6828930cClK5L/HK/zh"
   },
   {
-    id: "DittoFriends8",
+    id: "DttoFriends8",
     text: "dtto friends 表情貼 Dinu <1>",
     href: "https://whatsticker.online/p/682919SMvFJsn/HK/zh"
   },
   {
-    id: "DittoFriends9",
+    id: "DttoFriends9",
     text: "dtto friends 表情貼 Dinu <2>",
     href: "https://whatsticker.online/p/682920r3JqmqY/HK/zh"
   },
   {
-    id: "DittoFriends10",
+    id: "DttoFriends10",
     text: "dtto friends 表情貼 Hoya <1>",
     href: "https://whatsticker.online/p/6829212Pcccnb/HK/zh"
   },
   {
-    id: "DittoFriends11",
+    id: "DttoFriends11",
     text: "dtto friends 表情貼 Hoya <2>",
     href: "https://whatsticker.online/p/6829220jEVQD3/HK/zh"
   },
